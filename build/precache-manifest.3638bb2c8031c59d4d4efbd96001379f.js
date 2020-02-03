5d882e5a8e40d80a4f3171e2f75d25382c8e5483
Elias Ikävalko <elias.ikavalko99@gmail.com>
2020-02-03 13:13:31 +0200

    frontend herokuun

---
+++ build/precache-manifest.3638bb2c8031c59d4d4efbd96001379f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8c1a9d6bdf62cf68c73b643de74e00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb2ecaa908522ab39e71",
+    "url": "/static/js/2.98e8e06b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.98e8e06b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce560d2b44aa5dd5c580",
+    "url": "/static/js/main.9e1986c0.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
---
+++ build/precache-manifest.9e2ec9f49d052466aaf4113d73c5e792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eef88be800e73bfc1d64ade94d341b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb2ecaa908522ab39e71",
+    "url": "/static/js/2.98e8e06b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.98e8e06b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6bf66526b073c6bfec1",
+    "url": "/static/js/main.82a793ab.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
